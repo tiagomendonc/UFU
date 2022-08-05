f252ab3e703613806d7fa5ea429d837376f9336c
tiagomendonc <tiagomendoncahist@gemail.com>
2022-08-05 08:55:25 -0300

    Listas e Exercicios

--- ipc/Lista5/main.c
+++ ipc/Lista5/main.c
@@ -64,31 +64,71 @@ int entraMinuto() {
     while(1) {
         printf("Minuto: \n"); scanf("%d", &min);
         if(min < 0 || min > 59) {
-            printf("Minutos invalidos! Informe minutos entre 00 e 59");
+            printf("Minutos invalidos! Informe minutos entre 00 e 59:");
         } else {
             return min;
         }
     }
 }
 
-void verificaHorarios(Horario h) {
+void verificaHorarios(Horario *h) {
     int bissexto = 0;
-    h.ano = entraAno();
-    if (((h.ano) % 400 == 0) || (((h.ano) % 4 == 0) && ((h.ano) % 100 != 0))) {
+    h->ano = entraAno();
+    if (((h->ano) % 400 == 0) || (((h->ano) % 4 == 0) && ((h->ano) % 100 != 0))) {
         bissexto = 1;
     }
 
-    h.mes = entraMes(bissexto);
+    h->mes = entraMes(bissexto);
 
-    if(bissexto == 1 && h.mes == 2) {
-        h.dia = entraDia(29);
-    } else if(h.mes == 2) {
-        h.dia = entraDia(28);
-    } else if (h.mes == 1 || h.mes == 3 || h.mes == 1 || h.mes == 5 || h.mes == 7 || h.mes == 8 || h.mes == 10 || h.mes == 12) {
-        h.dia = entraDia(31);
+    if(bissexto == 1 && (h->mes) == 2) {
+        h->dia = entraDia(29);
+    } else if((h->mes) == 2) {
+        h->dia = entraDia(28);
+    } else if ((h->mes) == 1 || (h->mes) == 3 || (h->mes) == 1 || (h->mes) == 5 || (h->mes) == 7 || (h->mes) == 8 || (h->mes) == 10 || (h->mes) == 12) {
+        h->dia = entraDia(31);
     } else {
-        h.dia = entraDia(30);
+        h->dia = entraDia(30);
     }
+
+    h->hora = entraHora();
+    h->min = entraMinuto();
+}
+
+Horario calculaTempo(Horario hEntrada, Horario hSaida) {
+    Horario tempo;
+
+    tempo.ano = hSaida.ano - hEntrada.ano;
+    tempo.mes = hSaida.mes - hEntrada.mes;
+    if(hSaida.dia < hEntrada.dia) {
+        if (hEntrada.mes == 2 && ((hEntrada.ano) % 400 == 0) || (((hEntrada.ano) % 4 == 0) && ((hEntrada.ano) % 100 != 0))) {
+            tempo.dia = 29 - hEntrada.dia + hSaida.dia;
+        } else if (hEntrada.mes == 2) {
+            tempo.dia = 28 - hEntrada.dia + hSaida.dia;
+        } else if (hEntrada.mes == 1 || hEntrada.mes == 3 || hEntrada.mes == 5 || hEntrada.mes == 7 || hEntrada.mes == 8 || hEntrada.mes == 10 || hEntrada.mes == 12) {
+            tempo.dia = 31 - hEntrada.dia + hSaida.dia;
+        } else {
+            tempo.dia = 30 - hEntrada.dia + hSaida.dia;
+        }
+    } else {
+        tempo.dia = hSaida.dia - hEntrada.dia;
+    }
+
+    tempo.hora = hSaida.hora - hEntrada.hora;
+    tempo.min = hSaida.min - hEntrada.min;
+    if(tempo.min < 0) {
+        tempo.min += 60;
+        tempo.hora -= 1;
+    }
+    if(tempo.hora < 0) {
+        tempo.hora += 24;
+        tempo.dia -= 1;
+    }
+    if(tempo.mes < 0) {
+        tempo.mes += 12;
+        tempo.ano -= 1;
+    }
+
+    return tempo;
 }
 
 void inserir(FILE *f) {
@@ -97,9 +137,9 @@ void inserir(FILE *f) {
     printf("Informe o modelo do carro:\n"); scanf("%s", v1.modelo); getchar();
     printf("Informe a cor do carro:\n"); scanf("%s", v1.cor); getchar();
     printf("Informacoes sobre o horario de entrada:\n");
-    verificaHorarios(v1.entrada);
+    verificaHorarios(&v1.entrada);
     printf("\nAgora, insira informacoes do horario da SAIDA.\n");
-    verificaHorarios(v1.saida);
+    verificaHorarios(&v1.saida);
 
     v1.status = 'P';
 
@@ -112,6 +152,50 @@ void alterar(FILE *f) {
     struct cadastro v1;
     char placa[8];
     printf("Informe a placa do veiculo a ser alterado:\n"); scanf("%s",placa); getchar();
+
+    rewind(f);
+    while(fread(&v1, sizeof(v1), 1, f) > 0) {
+        if(strcmp(v1.placa, placa) == 0 && v1.status == 'P') {
+            printf("Registro encontrado!!\n");
+            int opcao = -1;
+            while(opcao != 0) {
+                printf("O que deseja alterar: \n");
+                printf(") - Sair\n");
+                printf("1 - Placa\n");
+                printf("2 - Modelo\n");
+                printf("3 - Cor\n");
+                printf("4 - Horario entrada\n");
+                printf("5 - Horario saida\n");
+                scanf("%d", &opcao);
+
+                switch(opcao) {
+                    case 1: printf("Informe a nova placa: \n"); scanf("%s", v1.placa); getchar();
+                            fseek(f, -1 * sizeof(v1), SEEK_CUR);
+                            fwrite(&v1, sizeof(veiculo), 1, f);
+                            break;
+                    case 2: printf("Informe o novo modelo: \n"); scanf("%s", v1.modelo); getchar();
+                            fseek(f, -1 *sizeof(v1), SEEK_CUR);
+                            fwrite(&v1, sizeof(veiculo), 1, f);
+                            break;
+                    case 3: printf("Informe a nova cor: \n"); scanf("%s", v1.cor); getchar();
+                            fseek(f, -1 * sizeof(v1), SEEK_CUR);
+                            fwrite(&v1, sizeof(veiculo), 1, f);
+                            break;
+                    case 4: printf("Informe o novo horario de entrada: \n");
+                            verificaHorarios(&v1.entrada);
+                            fseek(f, -sizeof(v1), SEEK_CUR);
+                            fwrite(&v1, sizeof(veiculo), 1, f);
+                            break;
+                    case 5: printf("Informe o novo horario de saida: \n");
+                            verificaHorarios(&v1.saida);
+                            fseek(f, -sizeof(v1), SEEK_CUR);
+                            fwrite(&v1, sizeof(veiculo), 1, f);
+                            break;
+                    printf("\n\n\n");
+                }
+            }
+        }
+    }
 }
 
 void remover(FILE *f) {
@@ -123,7 +207,8 @@ void remover(FILE *f) {
     while(fread(&v1, sizeof(v1), 1, f) > 0) {
         if(strcmp(v1.placa, placa) == 0) {
             v1.status = 'R';
-            fseek(f, -1 * sizeof(v1), SEEK_CUR);
+            fseek(f, -1 * sizeof(veiculo), SEEK_CUR);
+            fwrite(&v1, sizeof(veiculo), 1, f);
             printf("XXXXXXXXXXXXX VEICULO REMOVIDO XXXXXXXXXXXXX");
             break;
         }
@@ -143,11 +228,14 @@ void buscar(FILE *f) {
             printf("%d/%d/%d - %d:%d\n", v1.entrada.dia, v1.entrada.mes, v1.entrada.ano, v1.entrada.hora, v1.entrada.min);
             printf("SAIDA:\n");
             printf("%d/%d/%d - %d:%d\n", v1.saida.dia, v1.saida.mes, v1.saida.ano, v1.saida.hora, v1.saida.min);
+
+            Horario tempoEstacionado = calculaTempo(v1.entrada, v1.saida);
+            printf("Tempo estacionado: \n Anos: %d\n Meses: %d\n Dias: %d\n Horas: %d\n Minutos: %d\n", tempoEstacionado.ano, tempoEstacionado.mes, tempoEstacionado.dia, tempoEstacionado.hora, tempoEstacionado.min);
         }
     }
 }
 
-void listar(FILE *f) { // MESMO APOS REMOVIDO ARQUIVO CONTINUAR A SER LISTADO
+void listar(FILE *f) {
     struct cadastro v1;
     printf("Os registros ja incluidos sao:\n");
 
@@ -159,6 +247,11 @@ void listar(FILE *f) { // MESMO APOS REMOVIDO ARQUIVO CONTINUAR A SER LISTADO
             printf("%d/%d/%d - %d:%d\n", v1.entrada.dia, v1.entrada.mes, v1.entrada.ano, v1.entrada.hora, v1.entrada.min);
             printf("SAIDA:\n");
             printf("%d/%d/%d - %d:%d\n", v1.saida.dia, v1.saida.mes, v1.saida.ano, v1.saida.hora, v1.saida.min);
+
+            Horario tempoEstacionado = calculaTempo(v1.entrada, v1.saida);
+            printf("Tempo estacionado: \n Anos: %d\n Meses: %d\n Dias: %d\n Horas: %d\n Minutos: %d\n", tempoEstacionado.ano, tempoEstacionado.mes, tempoEstacionado.dia, tempoEstacionado.hora, tempoEstacionado.min);
+
+            printf("\n\n\n");
         }
     }
 }
